e2797aaf2472ec5108226f2d9f2021929854b681
Nick Arocho <nicaroch@amazon.com>
2022-06-16 14:41:25 -0700

    import, configure, and use `amazon-cognito-identity-js`

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./fancy-button.css";
+
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+} from "amazon-cognito-identity-js";
+
+import { useEffect, useState } from "react";
+
+const poolData = {
+  UserPoolId: "us-west-2_c1Dq5T2lY", // Your user pool id here
+  ClientId: "6oohg5pvj0cgphsu3at9nq7bol", // Your client id here
+};
+const userPool = new CognitoUserPool(poolData);
+
+console.log({ userPool });
+
+const attributeList = [];
+
+const dataEmail = {
+  Name: "email",
+  Value: "email@mydomain.com",
+};
+
+const dataPhoneNumber = {
+  Name: "phone_number",
+  Value: "+16024108498",
+};
+const attributeEmail = new CognitoUserAttribute(dataEmail);
+const attributePhoneNumber = new CognitoUserAttribute(dataPhoneNumber);
+
+attributeList.push(attributeEmail);
+attributeList.push(attributePhoneNumber);
 
 function App() {
+  const [user, setUser] = useState({});
+  const [signUpResult, setSignUpResult] = useState(null);
+
+  const username = `user-${Date.now()}`;
+
+  console.log({ username });
+
+  const signUp = () => {
+    userPool.signUp(
+      username,
+      "password",
+      attributeList,
+      null,
+      (err, result) => {
+        if (err) {
+          console.error(err.message || JSON.stringify(err));
+          setSignUpResult({
+            error: `Something went wrong: ${err.message}`,
+          });
+          return;
+        }
+        // var cognitoUser = result.user;
+        // console.log("user name is " + cognitoUser.getUsername());
+
+        console.log({ result });
+        setSignUpResult(result);
+        setUser(result.user);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-info">
+        {signUpResult && (
+          <>
+            <h3>Sign up result:</h3>
+            <p>
+              {signUpResult.error
+                ? signUpResult.error
+                : `Great success! ${signUpResult.user.username} is now signed up.`}
+            </p>
+          </>
+        )}
+      </div>
+      <button onClick={signUp}>Sign Up</button>
     </div>
   );
 }
